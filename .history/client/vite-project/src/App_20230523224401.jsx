cbc7a88864b7d198b2e53af730ac59a3a2c801c8
Atena Hatta <atn041102@gmail.com>
2023-05-23 22:44:21 -0700

    s

--- .history/client/vite-project/src/App_20230523224401.jsx
+++ .history/client/vite-project/src/App_20230523224401.jsx
@@ -14,7 +14,7 @@ function App() {
     <Route path='/' element={<Layout />} >
       <Route index element={<IndexPage />} />
       <Route path='/login' element={<LoginPage />} />
-      <Route path='/register' element={<Register />}/>
+      <Route path={'/register'} element={<Register />}/>
       </Route>
     </Routes>
   )
