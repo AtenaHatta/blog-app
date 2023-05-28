95911b8667f6e7766d0709cc6aa6ba9ade57d391
Atena Hatta <atn041102@gmail.com>
2023-05-28 13:46:52 -0700

    css

--- client/vite-project/src/pages/PostPage.jsx
+++ client/vite-project/src/pages/PostPage.jsx
@@ -1,9 +1,15 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useContext } from 'react';
 import { useParams } from 'react-router-dom';
+
+import { UserContext } from '../UserContext';
+
 import { formatISO9075 } from 'date-fns';
+import { FiEdit } from 'react-icons/fi';
+
 
 export default function PostPage() {
     const [postInfo, setPostInfo] = useState(null)
+    const {userInfo} = useContext(UserContext)
     const {id} = useParams()
 
     useEffect(() => { 
@@ -23,6 +29,12 @@ export default function PostPage() {
           <h1>{postInfo.title}</h1>
           <time>{formatISO9075(new Date(postInfo.createdAt))}</time>
           <div className="author">by {postInfo.author.username}</div>
+          {userInfo.id === postInfo.author._id &&(
+            <div className="edit-row">
+            
+            <a className="edit-btn" href=""><FiEdit /> Edit here</a>
+            </div>
+          )}
           <div className="image">
             <img src={`http://localhost:8000/${postInfo.cover}`} alt='' />
           </div>
