4399f3ec5ef06c7c53a04a27ae12ef30a546cca5
Atena Hatta <atn041102@gmail.com>
2023-05-28 19:02:43 -0700

    done coding

--- api/index.js
+++ api/index.js
@@ -1,8 +1,8 @@
+const Post = require("../api/src/models/Post.js"); 
 const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
 const User = require("../api/src/models/User.js");
-const Post = require("../api/src/models/Post.js");
 const bcrypt = require("bcryptjs");
 const app = express();
 const jwt = require("jsonwebtoken");
@@ -12,15 +12,14 @@ const secret = "vfdgdnhtnmnrhgrsfsf";
 const multer = require("multer"); //upload file
 const uploadMiddleware = multer({ dest: "uploads/" }); //upload file to "uploads" folder
 const fs = require("fs"); //file system
-const path = require('path');
+const path = require("path");
 const { log } = require("console");
 
 //cors allow to access "localhost:5173" to "localhost:8000"
 app.use(cors({ credentials: true, origin: "http://localhost:5173" }));
 app.use(express.json());
 app.use(cookieParser());
-app.use('/uploads',express.static(path.join(__dirname + '/uploads')));
-
+app.use("/uploads", express.static(path.join(__dirname + "/uploads")));
 
 //mongoDB's project code
 mongoose.connect(
@@ -46,23 +45,19 @@ app.post("/register", async (req, res) => {
 // Login -----------------------------
 app.post("/login", async (req, res) => {
   const { username, password } = req.body;
-
-  //check if username exists
   const userDoc = await User.findOne({ username });
-  const passOk = userDoc && bcrypt.compareSync(password, userDoc.password); // compare password
-
+  const passOk = bcrypt.compareSync(password, userDoc.password);
   if (passOk) {
     // logged in
-    jwt.sign({ username, id: userDoc._id }, secret, {}, async (err, token) => {
+    jwt.sign({ username, id: userDoc._id }, secret, {}, (err, token) => {
       if (err) throw err;
       res.cookie("token", token).json({
-        // Get userID and username
         id: userDoc._id,
         username,
       });
     });
   } else {
-    res.status(400).json("Wrong credentials");
+    res.status(400).json("wrong credentials");
   }
 });
 
@@ -80,39 +75,11 @@ app.post("/logout", (req, res) => {
   res.cookie("token", "").json("ok");
 });
 
-
 // Post update ------------------------
 app.post("/post", uploadMiddleware.single("file"), async (req, res) => {
-    let newPath = "";
-    
-    if (req.file) {
-      const { originalname, path } = req.file;
-      console.log(req.file);
-      const parts = originalname.split(".");
-      const ext = parts[parts.length - 1];
-      newPath = path + "." + ext;
-      fs.renameSync(path, path + "." + ext);
-    }
-    
-    const { token } = req.cookies;
-    jwt.verify(token, secret, {}, async (err, info) => {
-      if (err)throw err;
-      const { title, summary, content } = req.body;
-      const postDoc = await Post.create({
-        title,
-        summary,
-        content,
-        cover: newPath,
-        author: info.id, 
-      });
-      res.json(postDoc);
-    });
-  });
-  
+  let newPath = "";
 
-app.put("/post", uploadMiddleware.single("file"), async (req, res) => {
-  let newPath = null;
-  if(req.file){
+  if (req.file) {
     const { originalname, path } = req.file;
     console.log(req.file);
     const parts = originalname.split(".");
@@ -120,42 +87,70 @@ app.put("/post", uploadMiddleware.single("file"), async (req, res) => {
     newPath = path + "." + ext;
     fs.renameSync(path, path + "." + ext);
   }
-   const { token } = req.cookies;
-   jwt.verify(token, secret, {}, async (err, info) => {
 
-    
-    if (err)throw err;
-    const {id, title, summary, content} = req.body;
+  const { token } = req.cookies;
+  jwt.verify(token, secret, {}, async (err, info) => {
+    if (err) throw err;
+    const { title, summary, content } = req.body;
+    const postDoc = await Post.create({
+      title,
+      summary,
+      content,
+      cover: newPath,
+      author: info.id,
+    });
+    res.json(postDoc);
+  });
+});
+
+app.put("/post", uploadMiddleware.single("file"), async (req, res) => {
+  let newPath = null;
+  if (req.file) {
+    const { originalname, path } = req.file;
+    console.log(req.file);
+    const parts = originalname.split(".");
+    const ext = parts[parts.length - 1];
+    newPath = path + "." + ext;
+    fs.renameSync(path, path + "." + ext);
+  }
+  const { token } = req.cookies;
+  jwt.verify(token, secret, {}, async (err, info) => {
+    if (err) throw err;
+    const { id, title, summary, content } = req.body;
     const postDoc = await Post.findById(id);
     const isAuthor = JSON.stringify(postDoc.author) === JSON.stringify(info.id);
-    if(!isAuthor){
-      return res.status(400).json('you are not the author')
+    if (!isAuthor) {
+      return res.status(400).json("you are not the author");
     }
-    await postDoc.update({
-      title, 
-      summary, 
-      content,
-      cover: newPath ? newPath : postDoc.cover,
-    })
+    await Post.findOneAndUpdate(
+      { _id: id },
+      {
+        title,
+        summary,
+        content,
+        cover: newPath ? newPath : postDoc.cover,
+      }
+    );
     res.json(postDoc);
   });
 });
 
 // Get all posts -----------------------------
 app.get("/post", async (req, res) => {
-  res.json(await Post.find()
-     .populate('author', ['username']) // populate: connect to User model
-     .sort({createdAt: -1}) // sort: createdAt: -1 → show the latest post
-     .limit(20) // limit: show 20 posts
+  res.json(
+    await Post.find()
+      .populate("author", ["username"]) // populate: connect to User model
+      .sort({ createdAt: -1 }) // sort: createdAt: -1 → show the latest post
+      .limit(20) // limit: show 20 posts
   );
 });
 
-app.get('/post/:id', async (req, res) => {
+app.get("/post/:id", async (req, res) => {
   const { id } = req.params;
-  const postDoc = await Post.findById(id).populate('author', ['username']);
+  const postDoc = await Post.findById(id).populate("author", ["username"]);
   res.json(postDoc);
 });
 
 app.listen(8000, () => {
-    console.log("Server started");
-    });
+  console.log("Server started");
+});
--- client/vite-project/src/pages/EditPost.jsx
+++ client/vite-project/src/pages/EditPost.jsx
@@ -3,53 +3,50 @@ import { Navigate, useParams } from "react-router-dom";
 
 import Editor from "../Editor";
 
-
 export default function EditPost() {
-    const { id } = useParams()
-    const [title, setTitle] = useState("");
-    const [summary, setSummary] = useState("");
-    const [content, setContent] = useState("");
-    const [files, setFiles] = useState("");
-    const [redirect, setRedirect] = useState(false);
+  const { id } = useParams();
+  const [title, setTitle] = useState("");
+  const [summary, setSummary] = useState("");
+  const [content, setContent] = useState("");
+  const [files, setFiles] = useState("");
+  const [redirect, setRedirect] = useState(false);
 
-    // to show current post info data for editing
-    useEffect(() => {
-        fetch(`http://localhost:8000/post/${id}`)
-        .then(response => {
-            response.json().then(postInfo => {
-                setTitle(postInfo.title)
-                setSummary(postInfo.summary)
-                setContent(postInfo.content)
-            });
+  // to show current post info data for editing
+  useEffect(() => {
+    fetch("http://localhost:8000/post/" + id).then((response) => {
+      response.json().then((postInfo) => {
+        setTitle(postInfo.title);
+        setContent(postInfo.content);
+        setSummary(postInfo.summary);
+      });
     });
-},[]);
-    
-    async function updatePost(e){
-        e.preventDefault();
-        const data = new FormData();
-        data.set("title", title);
-        data.set("summary", summary);
-        data.set("content", content);
-        data.set("id", id);
-        if(files?.[0]){
-            data.set("file", files?.[0]);
-        }
-        const response = await fetch(`http://localhost:8000/post/${id}`, {
-          method: 'PUT',
-          body: data,
-          credentials: 'include',
-        })
-        if(response.ok){
-            setRedirect(true)
-        }
-    }
+  }, []);
 
-    if(redirect){
-        return <Navigate to={'/post/'+id} />
+  async function updatePost(ev) {
+    ev.preventDefault();
+    const data = new FormData();
+    data.set("title", title);
+    data.set("summary", summary);
+    data.set("content", content);
+    data.set("id", id);
+    if (files?.[0]) {
+      data.set("file", files?.[0]);
     }
+    const response = await fetch("http://localhost:8000/post", {
+      method: "PUT",
+      body: data,
+      credentials: "include",
+    });
+    if (response.ok) {
+      setRedirect(true);
+    }
+  }
 
-  
- return (
+  if (redirect) {
+    return <Navigate to={"/post/" + id} />;
+  }
+ 
+  return (
     <form onSubmit={updatePost}>
       <input
         type="title"
@@ -65,9 +62,7 @@ export default function EditPost() {
       />
       <input type="file" onChange={(e) => setFiles(e.target.files)} />
       <Editor value={content} onChange={setContent} />
-      <button type="submit">
-        Update post
-      </button>
+      <button type="submit">Update post</button>
     </form>
   );
 }
