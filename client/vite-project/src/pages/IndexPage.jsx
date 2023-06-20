f436f9745c7dfd4f247b09bc65bbe476fd78a0cc
Atena Hatta <atn041102@gmail.com>
2023-06-19 21:20:15 -0700

    ADDED -favicon

--- client/vite-project/src/pages/IndexPage.jsx
+++ client/vite-project/src/pages/IndexPage.jsx
@@ -6,8 +6,6 @@ import Post from '../Post'
 export default function indexPage(){
   const [posts, setPosts] = useState([]) 
 
-  console.log(posts);
-
     //Connect to Backend
     useEffect(() => {
         fetch('http://localhost:8000/post').then(response => {
--- client/vite-project/src/pages/PostPage.jsx
+++ client/vite-project/src/pages/PostPage.jsx
@@ -1,45 +1,67 @@
-import { useState, useEffect, useContext } from 'react';
-import { useParams, Link } from 'react-router-dom';
+import { useState, useEffect, useContext } from "react";
+import { useParams, Link, useNavigate } from "react-router-dom";
 
-import { UserContext } from '../UserContext';
+import { UserContext } from "../UserContext";
 
-import { formatISO9075 } from 'date-fns';
-import { FiEdit } from 'react-icons/fi';
-import { AiTwotoneDelete } from 'react-icons/ai';
+import { formatISO9075 } from "date-fns";
+import { FiEdit } from "react-icons/fi";
+import { AiTwotoneDelete } from "react-icons/ai";
 
 export default function PostPage() {
-    const [postInfo, setPostInfo] = useState(null)
-    const {userInfo} = useContext(UserContext)
-    const {id} = useParams()
+  const navigate = useNavigate();
+  const [postInfo, setPostInfo] = useState(null);
+  const { userInfo } = useContext(UserContext);
+  const { id } = useParams();
 
-    useEffect(() => { 
-    console.log(id);
-      fetch(`http://localhost:8000/post/${id}`)
-      .then(response => {
-        response.json().then(postInfo => {
-            setPostInfo(postInfo)
-        })
+  useEffect(() => {
+    fetch(`http://localhost:8000/post/${id}`).then((response) => {
+      response.json().then((postInfo) => {
+        setPostInfo(postInfo);
+      });
+    });
+  }, []);
+
+  if (!postInfo) return "";
+
+  // Delete
+  const handleDlete = () => {
+    fetch(`http://localhost:8000/post/${id}`, {
+      method: "POST",
+      credentials: "include",
+    })
+      .then((response) => {
+        navigate("/");
       })
-    },[])
+      .catch((error) => {
+        console.error("Error deleting post:", error);
+      });
+  };
 
-    if(!postInfo) return '';
-
-    return(
-        <div className="post-page">
-            {userInfo.id === postInfo.author._id &&(
-            <div className="edit-row">
-              <Link className="edit-btn" to={`/edit/${postInfo._id}`}><FiEdit />Edit</Link>
-              <Link className="edit-btn" to={`/delete/${postInfo._id}`}><AiTwotoneDelete />Delete</Link>
-            </div>
-            )}
-          <h1>{postInfo.title}</h1>
-          <time>{formatISO9075(new Date(postInfo.createdAt))}</time>
-          <div className="author">by {postInfo.author.username}</div>
- 
-          <div className="image">
-            <img src={`http://localhost:8000/${postInfo.cover}`} alt='' />
-          </div>
-          <div className="content" dangerouslySetInnerHTML={{__html:postInfo.content}} /> 
+  return (
+    <div className="post-page">
+      {userInfo.id === postInfo.author._id && (
+        <div className="edit-row">
+          <Link className="edit-btn" to={`/edit/${postInfo._id}`}>
+            <FiEdit />
+            Edit
+          </Link>
+          <a className="edit-btn" onClick={handleDlete}>
+            <AiTwotoneDelete />
+            Delete
+          </a>
         </div>
-    )
+      )}
+      <h1>{postInfo.title}</h1>
+      <time>{formatISO9075(new Date(postInfo.createdAt))}</time>
+      <div className="author">by {postInfo.author.username}</div>
+
+      <div className="image">
+        <img src={`http://localhost:8000/${postInfo.cover}`} alt="" />
+      </div>
+      <div
+        className="content"
+        dangerouslySetInnerHTML={{ __html: postInfo.content }}
+      />
+    </div>
+  );
 }
